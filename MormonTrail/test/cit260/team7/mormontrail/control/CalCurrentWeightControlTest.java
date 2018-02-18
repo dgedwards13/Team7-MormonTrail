dc7e95ef59ecbbfb95268acdd4849b0e3e2476d7
shawcode <36020704+shawcode@users.noreply.github.com>
2018-02-17 18:16:00 -0700

    2/17/18

--- MormonTrail/test/cit260/team7/mormontrail/control/CalCurrentWeightControlTest.java
+++ MormonTrail/test/cit260/team7/mormontrail/control/CalCurrentWeightControlTest.java
@@ -23,67 +23,36 @@ public class CalCurrentWeightControlTest {
     @Test
     public void testCalCurrentWeight() {
         System.out.println("\tTest case 1");
-        double invItemWeight =0;
         double wagonSize = 1000;
-        double expResult = 40;
-        double result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
+        double expResult = 54;
+        double result = CalCurrentWeightControl.calCurrentWeight(wagonSize);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
 
-//System.out.println("\tTest case 2");
-//         invItemWeight = 800;
-//         wagonSize = 1500;
-//         expResult = 800;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
-//
-//System.out.println("\tTest case 3");
-//         invItemWeight = 500;
-//         wagonSize = 2000;
-//         expResult = 500;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
-//
-//System.out.println("\tTest case 4");
-//         invItemWeight = -3;
-//         wagonSize = 1000;
-//         expResult = -1;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
-//
-//System.out.println("\tTest case 5");
-//         invItemWeight = 2000;
-//         wagonSize = 1500;
-//         expResult = -1;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
-//
-//System.out.println("\tTest case 6");
-//         invItemWeight = 2031;
-//         wagonSize = 2000;
-//         expResult = -1;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
-//
-//System.out.println("\tTest case 7");
-//         invItemWeight = 0;
-//         wagonSize = 0;
-//         expResult = 0;
-//         result = CalCurrentWeightControl.calCurrentWeight(invItemWeight, wagonSize);
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-////        fail("The test case is a prototype.");
+System.out.println("\tTest case 2");
+        wagonSize = 50;
+        expResult = -1;
+        result = CalCurrentWeightControl.calCurrentWeight(wagonSize);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+
+System.out.println("\tTest case 3");
+        wagonSize = 54;
+        expResult = 54;
+        result = CalCurrentWeightControl.calCurrentWeight(wagonSize);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+
+System.out.println("\tTest case 4");
+        wagonSize = 30;
+        expResult = -1;
+        result = CalCurrentWeightControl.calCurrentWeight(wagonSize);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
     }
     
 }
