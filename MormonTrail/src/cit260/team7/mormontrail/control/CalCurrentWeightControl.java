89198eb83085e663d7e3eb9f2121741d6264a47d
shawcode <36020704+shawcode@users.noreply.github.com>
2018-02-17 18:15:37 -0700

    2/17/18

--- MormonTrail/src/cit260/team7/mormontrail/control/CalCurrentWeightControl.java
+++ MormonTrail/src/cit260/team7/mormontrail/control/CalCurrentWeightControl.java
@@ -9,28 +9,40 @@ package cit260.team7.mormontrail.control;
  *
  * @author Shaw-Laptop
  */
-
+import cit260.team7.mormontrail.model.InventoryItem;
 
         
 public class CalCurrentWeightControl {
-    public static double calCurrentWeight(double invItemWeight, double wagonSize){
+    public static double calCurrentWeight( double wagonSize){
         //currentWeight = invItemWeight + invItemWeight - wagonSize;
         double currentWeight = 0;
         
        
-        invItemWeight = 0;
-       
-        //will be replaced by inventory list
-       double invArray[]={10,10,10,10};
-         
-     int invCount = invArray.length;
-    
-     int i = 0;
-     
-       for(i = 0; i < invCount;i++){
-          invItemWeight = invItemWeight + invArray[i];           
-     }
-      
+//        invItemWeight = 0;
+//       
+//        //will be replaced by inventory list
+//       double invArray[]={10,10,10,10};
+//         
+ 
+//    
+//     int i = 0;
+//     
+//       for(i = 0; i < invCount;i++){
+//          invItemWeight = invItemWeight + invArray[i];           
+//     }
+
+InventoryControl.setInventory();
+
+InventoryItem[] inventoryArray = InventoryControl.getInventory();
+ int invCount = inventoryArray.length;      
+
+      double invItemWeight = 0;
+      for (int i= 0; i < invCount; i++){
+      double w = inventoryArray[i].getItemWeight();
+      double a = inventoryArray[i].getAmount();
+      double wa = w * a;
+      invItemWeight= invItemWeight + wa;
+      }
       
         if(invItemWeight < 0 || invItemWeight > wagonSize){
             currentWeight = -1;
