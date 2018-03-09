b0301fcbc7817d6d3a6557f515bd6c15a2e14184
rherrerajr <rherrerajr@192.168.1.12>
2018-03-09 00:20:24 -0500

    DailyTrailStopSceneView with super iheritance

--- MormonTrail/src/cit260/team7/mormontrail/view/DailyTrailStopSceneView.java
+++ MormonTrail/src/cit260/team7/mormontrail/view/DailyTrailStopSceneView.java
@@ -13,67 +13,30 @@ import cit260.team7.mormontrail.control.LocationControl;
  *
  * @author rherrerajr
  */
-public class DailyTrailStopSceneView {
+public class DailyTrailStopSceneView extends View {
+//    DailyTrailStopSceneView dailyTrailStop = new DailyTrailStopSceneView();
+//    dailyTrailStop.display();
     
     public DailyTrailStopSceneView(){
-         boolean endOfView = false;
-          do{
-              String[] inputs = this.getInputs();
-                      if (inputs == null || inputs.length < 1){
-                          return;
-                      }
-                      endOfView = doAction(inputs);
-          }
-          while (endOfView != true);
+        
+        super( "\n"
+            +"\n================================================================================"
+            +"\n==                                                                            =="
+            +"\n==                              Daily Stop                                    =="
+            +"\n==                                                                            =="
+            +"\n================================================================================"
+            +"\n1" + LocationControl.nearby()
+            +"\n2| Rest for the day"
+            +"\n3| Go hunting"
+            +"\n4| Look for edible plants"
+            +"\n5| Exit"
+            +"\n------------------------------------------", 5);
     }
     
-    private String[] getInputs() {
-        String[] inputs = new String[1];
-        boolean valid = false;
+  @Override
+    public boolean doAction(String inputs) {
         
-        while (valid == false) {
-            
-            Scanner keyboard = new Scanner(System.in);
-            
-            System.out.println("Daily Trail Stop");
-            String nearby = LocationControl.nearby();
-            if (nearby.equals("town")) {
-                System.out.println("1 | Visit town");
-            } else if (nearby.equals("fort")){
-                System.out.println("1 | Visit Fort");
-            } else if (nearby.equals("river")){
-                System.out.println("1 | Visit River");
-            }
-            System.out.println("2 | Rest for the Day");
-            System.out.println("3 | Go Hunting");
-            System.out.println("4 | Look for Edible Plants");
-            System.out.println("5 | Exit Build");
-            String in = keyboard.nextLine();
-            
-            in = in.trim();
-            
-            if(in.length() < 1){
-                System.out.println("You must enter a non-blank value");
-                continue;
-            }
-            
-            boolean v = in.equalsIgnoreCase("1") || in.equalsIgnoreCase("2") || in.equalsIgnoreCase("3") || in.equalsIgnoreCase("4") || in.equalsIgnoreCase("5");
-                    
-            if(v != true) {
-                System.out.println("The number entered must correlate with the menu items.");
-                continue;
-            }
-            
-            inputs[0] = in;
-            
-            valid = true;
-        }
-        return inputs;
-    }
-
-    private boolean doAction(String[] inputs) {
-        
-        switch(inputs[0]) {
+        switch(inputs) {
             case "1":
                 System.out.println("Visit the " + LocationControl.nearby());
                 System.out.println("Welcome to " + LocationControl.nearby());
@@ -99,18 +62,10 @@ public class DailyTrailStopSceneView {
                 dailyTrailStop.display();
                 break;     
             case "5":
-                break;
+//                return true;
         }
            
-        return true;
+        return true; //return false;
     }
-    
-    public void display() {
-        
-    }
+  
 }
-
-    
-
-    
-
