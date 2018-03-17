b1c9984e7f228edfe28d3dc92588f0059ce0185f
dgedw <dgedw@DESKTOP-10NB2O1>
2018-03-17 16:11:52 -0600

    Testing Sort

---
+++ MormonTrail/src/cit260/team7/mormontrail/control/TestingSort.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.team7.mormontrail.control;
+
+import cit260.team7.mormontrail.model.InventoryItem;
+import java.util.Arrays;
+
+/**
+ *
+ * @author dgedw
+ */
+public class TestingSort {
+    
+    public static void main(String[] args) {
+        InventoryControl.setInventory();
+        InventoryControl.changeInventory("Food", 26, true);
+        InventoryControl.changeInventory("Oxen", 2, true);
+        InventoryControl.changeInventory("Ammunition", 14, true);
+        InventoryControl.changeInventory("Spare Parts", 3, true);
+        InventoryControl.changeInventory("Small Wagon", 1, true);
+        InventoryItem[] arr = InventoryControl.getInventory();
+        System.out.println("With no Sorting:");
+        for (int i= 0; i < arr.length; i++) {
+            System.out.println(arr[i].getItem() + " " + arr[i].getAmount());
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                InventoryItem tmp;
+                if (arr[i].getAmount() > arr[j].getAmount()) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.println("With Sorting:");
+        for (int i= 0; i < arr.length; i++) {
+            System.out.println(arr[i].getItem() + " " + arr[i].getAmount());
+        }
+    } 
+
+}
