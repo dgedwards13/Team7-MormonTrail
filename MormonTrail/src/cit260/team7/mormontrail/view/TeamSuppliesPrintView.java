bbfd88ebabe01d748e074a7e9c457e1485139757
shawcode <36020704+shawcode@users.noreply.github.com>
2018-03-31 21:56:50 -0700

    3/31/18

---
+++ MormonTrail/src/cit260/team7/mormontrail/view/TeamSuppliesPrintView.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.team7.mormontrail.view;
+
+
+import cit260.team7.mormontrail.control.InventoryControl;
+import cit260.team7.mormontrail.exception.InventoryException;
+import cit260.team7.mormontrail.model.InventoryItem;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+
+/**
+ *
+ * @author Shaw-Laptop
+ */
+public class TeamSuppliesPrintView extends View{
+        public TeamSuppliesPrintView() throws InventoryException {
+        super("\n"
+                + "\n================================================================================"
+                + "\n==                             Team Supplies                                  =="
+                + "\n================================================================================"
+                + "\n" + InventoryControl.order()
+                + "\n1 | return to game play", 1, true);
+    }
+
+    private static PrintWriter createFile(String fileName){
+     try{
+            
+            File listOfItems = new File(fileName);
+            
+            PrintWriter infoToWrite = new PrintWriter( 
+                    new BufferedWriter(
+                    new FileWriter(listOfItems)));
+            
+             
+        }
+        catch(IOException e){
+            System.out.println("Error");
+            System.exit(0);
+        
+        }
+        
+        return null;
+        
+    }
+
+    @Override
+    public boolean doAction(String inputs){
+     
+     InventoryItem[] inventoryArray = InventoryControl.getInventory(); 
+     
+     PrintWriter invOutput = createFile("teamSupplies.txt");
+     
+     for (InventoryItem inv : inventoryArray){
+     inv =+ "\n" + inventoryArray[].getitem()+ inventoryArray[].getAmount();
+     }
+        return true;
+       
+    }
+
+   
+}
+      
+        
+    
+
--- MormonTrail/src/cit260/team7/mormontrail/view/TeamSuppliesView.java
+++ MormonTrail/src/cit260/team7/mormontrail/view/TeamSuppliesView.java
@@ -8,6 +8,7 @@ package cit260.team7.mormontrail.view;
 import cit260.team7.mormontrail.control.InventoryControl;
 import cit260.team7.mormontrail.exception.InventoryException;
 
+
 /**
  *
  * @author Shaw-Laptop
@@ -20,8 +21,8 @@ public class TeamSuppliesView extends View {
                 + "\n==                             Team Supplies                                  =="
                 + "\n================================================================================"
                 + "\n" + InventoryControl.order()
-                + "\n1 | return to game play",
-                1, true);
+                + "\n1 | return to game play"
+                + "\n2 | Print Supply list", 2, true);
     }
 
     @Override
@@ -31,7 +32,15 @@ public class TeamSuppliesView extends View {
                 GamePlayView gamePlayView = new GamePlayView();
                 gamePlayView.display();
                 break;
-
+            case"2":
+                TeamSuppliesPrintView teamSuppliesPrintView = null;
+        try {
+            teamSuppliesPrintView = new TeamSuppliesPrintView();
+        } catch (InventoryException e) {
+           this.console.println(e.getMessage());
+        }
+                teamSuppliesPrintView.display();
+                break;
         }
         return true;
     }
