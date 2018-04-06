a1857db78023223034e31e702dc31cd556361ba6
fishsava <fis15014@byui.edu>
2018-04-06 10:26:48 -0700

    Events Print Working

--- MormonTrail/src/cit260/team7/mormontrail/view/EventsPrintView.java
+++ MormonTrail/src/cit260/team7/mormontrail/view/EventsPrintView.java
@@ -16,11 +16,11 @@ import java.lang.String;
  */
 public class EventsPrintView extends View{
     
-    public EventsPrintView() {
-        super("================================================================================"
-                + "==                                 Events                                     =="
-                + "==                                                                            =="
-                + "================================================================================"
+    public EventsPrintView() throws EventException {
+        super("\n"
+                + "\n================================================================================"
+                + "\n==                    Printing List of all Possible Events                    =="
+                + "\n================================================================================"
                 + "\n\n1 | Print List of Events"
                 , 1, true);
     }
@@ -28,25 +28,45 @@ public class EventsPrintView extends View{
 
         public static PrintWriter createFile(String fileName){
         try{
+            FileWriter fw = new FileWriter("savListEvents.txt");
+            Event[] eventArray = EventControl.getEvents();
+            
+            for (int i=0; i < eventArray.length; i++) {
+                fw.write("{ Event: " + eventArray[i].getDescription() + " Character Impact: " + eventArray[i].getCharImpact() + " Character Health Impact: " + eventArray[i].getCharHealthImpact() + " Oxen Health Impact: " + eventArray[i].getOxenHealthImpact() + " Inventory Impact: " + eventArray[i].getInventoryImpact() + " Inventory Amount Impact: " + eventArray[i].getInventoryAmountImpact() + "Distance Impact: " + eventArray[i].getDistanceImpact() + " Pace Impact: " + eventArray[i].getPaceImpact() + " }\n\n\n");
+            }
+            fw.close();
+            
             File listOfEvents = new File(fileName);
+            
             PrintWriter infoToWrite = new PrintWriter(
                     new BufferedWriter(
                             new FileWriter(listOfEvents)));
+            System.out.println("File Successfully Written");
+            
         }catch(IOException e){   
-            System.out.println("Failed to Write" + "\n Error code:" + e.getMessage());
-            System.exit(0);
-//            return false;
+                System.out.println("Failed to Write" + "\n Error code:" + e.getMessage());
+
+                EventsPrintView eventsPrintView = null;
+                try {
+                    eventsPrintView = new EventsPrintView();
+                } catch (EventException ex){
+                    System.out.println(ex.getMessage());
+                }
         }
-//        this.console.println("File Successfully Written");
+
         return null;
     }
         
-    
+    @Override
     public boolean doAction(String inputs){
         
-                 PrintWriter eventOutput = createFile("/cit260/team7/mormontrail/printpages/eventlogfile.txt");
+        Event[] eventArray = EventControl.getEvents();
+        PrintWriter eventOutput = createFile("eventfile.txt");
+        
+        GamePlayView gamePlayView = new GamePlayView();
+        gamePlayView.display();
 
-                return false;
+                return true;
 //            }catch(EventException e){
 //                this.console.println("Error " + e.getMessage());
 //            }
